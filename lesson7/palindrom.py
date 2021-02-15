b81d419ed2f03915e54c981f8956b3742282b318
Viktorika-p <77969607+Viktorika-p@users.noreply.github.com>
2021-02-15 18:23:26 +0200

    Create palindrom.py

---
+++ lesson7/palindrom.py
@@ -0,0 +1,16 @@
+# palindrom.py
+slovo = str(input("напишіть слово: "))
+x = len(slovo)
+i = 0
+x = x - 1
+k = 0
+while x - i >= i:
+    if slovo[x - i] == slovo[i]:
+        i += 1
+    else:
+        k = 1
+        break
+if k == 1:
+  print("no")
+else:
+  print("yes")
