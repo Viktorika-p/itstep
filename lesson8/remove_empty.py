096328da845a18cd27d530cd7365ac20ac435906
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-03-21 23:04:34 +0200

    Update remove_empty.py

--- lesson8/remove_empty.py
+++ lesson8/remove_empty.py
@@ -11,5 +11,24 @@ print(c)
 
 even = []
 d = a + b + c
-filter(lambda x: len(x) > 0, d)
-print(d)
+res = [ele for ele in d if ele != []]
+print(res)
+
+
+#варіант 2
+
+a = []
+for i in range(1, 10):
+    a.append(i)
+print(a)
+
+b = [i for i in range(1,10)]
+print(b)
+
+c = [i*2 for i in range(1,10)]
+print(c)
+
+even = []
+d = a + b + c
+res = list(filter(None, d))
+print(res)
