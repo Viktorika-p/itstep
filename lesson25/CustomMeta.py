131645603f1e58e534bcc26f2b86d8dc73756519
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-04-19 12:10:36 +0300

    Create CustomMeta.py

---
+++ lesson25/CustomMeta.py
@@ -0,0 +1,17 @@
+class CustomMeta(type):
+
+    def __new__(mcs, classname, method, attrs):
+        attributes = {}
+        for classname, value in attrs.items():
+            attributes[classname.lower() + ("method")] = value
+        return type(classname, method, attrs)
+
+
+class Test(metaclass=CustomMeta):
+    name = "Some_name"
+    def get_name(self):
+        print(self.name)
+
+if __name__ == "__main__":
+    a = Test()
+    print(a)
