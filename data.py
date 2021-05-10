c152e80f87bbb88149e13150770e983049d6bcb2
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-05-10 22:09:55 +0300

    Update data.py

--- data.py
+++ data.py
@@ -12,12 +12,20 @@ cur.execute('''CREATE TABLE peoples(
         birth INTEGER DATETIME);''')
 
 
-while True:
-    d = input("add last, sec, city, country and birth")
-    add = cur.execute(f"INSERT INTO peoples (last_name, sec_name, city, country, birth) VALUES {d};")
-    print(add)
-else:
-    break
+test_1 = cur.execute("SELECT * FROM peoples")
+print(test_1)
+for line in test_1:
+    print(line)
+
+add = input()
+cur.executemany('''INSERT INTO peoples (last_name, sec_name, city, country, birth) VALUES''', add)
+
+#while True:
+  #  d = input("add last, sec, city, country and birth")
+  #  add = cur.execute(f"INSERT INTO peoples (last_name, sec_name, city, country, birth) VALUES {d};")
+  #  print(add)
+#else:
+  #  break
 
 con.commit()
 con.close()
