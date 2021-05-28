44b366c3655dbdbcf03482bf27599470c3ce0326
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-05-28 09:46:30 +0300

    Create base.py

---
+++ lesson34/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+engine = create_engine('sqlite:///database.sqlite', echo=True)
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base() 
